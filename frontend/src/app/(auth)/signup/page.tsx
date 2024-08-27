a5698941d00cec0c5e15d12de629cd7e25e51fef
captainTigerYT <raunakmanna43@gmail.com>
2024-08-27 18:09:28 +0530

    minor changes

--- frontend/src/app/(auth)/signup/page.tsx
+++ frontend/src/app/(auth)/signup/page.tsx
@@ -9,7 +9,6 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Alert } from "@/components/ui/alert";
