229d7dd05aea8e3e096b2520f8c3b48196ab2bd6
Sagnik Chakraborty <sagnify2022@gmail.com>
2024-09-03 17:29:46 +0530

    Minor changes with DRF token

--- backend/main/authentication.py
+++ backend/main/authentication.py
@@ -1,10 +1,30 @@
+# from rest_framework.authentication import BaseAuthentication
+# from rest_framework.exceptions import AuthenticationFailed
+# from rest_framework_simplejwt.tokens import AccessToken
+# from rest_framework_simplejwt.exceptions import InvalidToken, TokenBackendError
+# from django.contrib.auth import get_user_model
+# from rest_framework.authtoken.models import Token
+
+
+# class QueryParamTokenAuthentication(BaseAuthentication):
+#     def authenticate(self, request):
+#         token_key = request.query_params.get('token')  # Get token from query parameters
+#         if not token_key:
+#             return None
+
+#         try:
+#             # Retrieve the token and corresponding user
+#             token = Token.objects.get(key=token_key)
+#             return (token.user, token)
+#         except Token.DoesNotExist:
+#             raise AuthenticationFailed('Invalid token.')
+
+#     def authenticate_header(self, request):
+#         return 'Token'
+
 from rest_framework.authentication import BaseAuthentication
 from rest_framework.exceptions import AuthenticationFailed
-from rest_framework_simplejwt.tokens import AccessToken
-from rest_framework_simplejwt.exceptions import InvalidToken, TokenBackendError
-from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
-
 
 class QueryParamTokenAuthentication(BaseAuthentication):
     def authenticate(self, request):
@@ -13,10 +33,8 @@ class QueryParamTokenAuthentication(BaseAuthentication):
             return None
 
         try:
-            # Retrieve the token and corresponding user
             token = Token.objects.get(key=token_key)
         except Token.DoesNotExist:
             raise AuthenticationFailed('Invalid token.')
 
         return (token.user, token)
-    
