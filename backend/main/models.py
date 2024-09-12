34572f07ea4034e68d268b16a7c638c50ef891e3
Sagnik Chakraborty <sagnify2022@gmail.com>
2024-09-12 08:36:13 +0530

    minor changes

--- backend/main/models.py
+++ backend/main/models.py
@@ -145,7 +145,7 @@ class Lecture(models.Model):
     asset_sel = models.ForeignKey(LibAsset, on_delete=models.CASCADE, blank=True, null=True)
     rating = models.DecimalField(max_digits=3, decimal_places=2, default=0.0)
     visibility = models.BooleanField(default=True)
-    is_verified = models.BooleanField(default=True)
+    is_verified = models.BooleanField(default=False)
     series = models.ForeignKey(Series, on_delete=models.CASCADE, related_name='lectures')
 
     def __str__(self):
