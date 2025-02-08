ad114fcc177bfc882e410c21a655731a9683afd3
Sagnik Chakraborty <150990247+Sagnify@users.noreply.github.com>
2025-02-08 23:45:18 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -51,7 +51,7 @@ SECRET_KEY = 'django-insecure-o8vfo6!&p)i^!yu+zdc7vp%5ka)@25voq^jyf3fb4buo(@^m54
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://edu-verse-wymf.vercel.app/']
 
 
 # Application definition
